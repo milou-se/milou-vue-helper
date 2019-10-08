28d59f3cda95382074415a49235ecd0e89addf00
Pontus Olofsson <pontus.olofsson@milou.se>
2019-10-08 23:39:13 +0200

    Adding export from root file

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+export {ArrayHasItems} from './helpers/array/arrayHasItems';
+export {ArrayMatch} from './helpers/array/arrayMatch';
+export {QueryToArray} from './helpers/array/queryToArray';
+
+export {IsMobile} from './helpers/browser/isMobile';
+
+export {Capitalize} from './helpers/convert/capitalize';
+export {FloatToString} from './helpers/convert/floatToString';
+export {StringToFloat} from './helpers/convert/stringToFloat';
+export {TitleCase} from './helpers/convert/titleCase';
+
+export {GenerateUniqueId} from './helpers/generate/generateUniqueId';
+
+export {ElementOffsetTop} from './helpers/html/elementOffsetTop';
+export {SanitizeHTML} from './helpers/html/sanitizeHtml';
+
+export {ObjectClone} from './helpers/object/objectClone';
+export {ObjectIsPlain} from './helpers/object/objectIsPlain';
+export {ObjectsAreEqual} from './helpers/object/objectsAreEqual';
+export {ObjectToQuery} from './helpers/object/objectToQuery';
+export {QueryToObject} from './helpers/object/queryToObject';
+
+export {Scrolling} from './helpers/scrolling/scrolling';
+
+export {UrlQueryParam} from './helpers/url/urlQueryParam';
+
+export {IsInsideViewport} from './helpers/viewport/isInsideViewport';
+export {IsOutsideViewport} from './helpers/viewport/isOutsideViewport';
+
+export {AppendComponent} from './helpers/vue/appendComponent';
+export {MountVueHtml} from './helpers/vue/mountVueHtml';
